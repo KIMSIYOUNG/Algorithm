9bb8fffbee2db66af5c16861384d0f680524b526
KIMSIYOUNG <tldud2404@gmail.com>
2019-09-23 12:24:26 +0900

    Baekjoon Math

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,19 @@
+import com.sun.org.apache.xml.internal.utils.IntStack;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println('a'=='a');
+        int a= 0;
+        while(true){
+            a++;
+            System.out.println(a);
+
+        }
     }
 }

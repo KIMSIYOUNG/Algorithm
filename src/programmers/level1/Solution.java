03ec878c95d7dca3e33fdaeedbfa2e21e7f8fd96
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-16 19:12:09 +0900

    Level 1 ) 완주하지 못한 선수

--- src/programmers/level1/Solution.java
+++ src/programmers/level1/Solution.java
@@ -1,27 +1,23 @@
 package programmers.level1;
 
-import java.util.Scanner;
+
+import java.util.Arrays;
 
 class Solution {
 
-    public int solution(int n) {
-        int answer = 0;
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
 
-        //주어진 숫자의 1/2 이상 값은 약수가 될 수 없다.
-        for(int i=1; i<=n/2; i++){
-            if(n%i==0){
-                answer += i;
+        for (i = 0; i < completion.length; i++) {
+
+            if (!(participant[i].equals(completion[i]))) {
+                return participant[i];
+
             }
         }
-        return answer+n;
-    }
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
-        Solution s = new Solution();
-        System.out.println(s.solution(input));
-
+        return participant[i];
     }
 }
+

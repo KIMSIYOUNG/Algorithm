3c44d041e1a90b42f4132d4ba4c9bf2918ba8aff
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-18 18:25:35 +0900

    람다로 제일 작은 수 제거하기

--- src/programmers/level1/Solution.java
+++ src/programmers/level1/Solution.java
@@ -1,26 +1,12 @@
 package programmers.level1;
 
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr) {
-        List<Integer> test = new ArrayList<>();
-        if(arr.length==1)
-            return new int[]{-1};
-        //TODO 선언한 리스트에 담기(기능 이용하려고)
-        for(int i=0; i<arr.length; i++)
-            test.add(arr[i]);
-        //TODO 최소값 구하고 리스트에서 제거
-        Integer min = Collections.min(test);
-        test.remove(min);
-        //TODO 다시 인트형 배열에 담아서 리턴
-        int answer[] = new int[test.size()];
-        for(int i=0; i<test.size(); i++)
-            answer[i] = test.get(i);
-
-        return answer;
+        if(arr.length==1) return new int[]{-1};
+        int min = Arrays.stream(arr).min().getAsInt();
+        return Arrays.stream(arr).filter(i->i!=min).toArray();
     }
 }

d25a6977092dee09352ba241a06475665cff6d7b
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-18 17:43:22 +0900

    정수 내림차순으로 배치하기

--- src/programmers/level1/Solution.java
+++ src/programmers/level1/Solution.java
@@ -1,21 +1,23 @@
 package programmers.level1;
 
-
 import java.util.Arrays;
 import java.util.Comparator;
 
 class Solution {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(Solution.solution(12379)));
+        System.out.println(Solution.solution(4953123123450123l));
     }
-    public static int[] solution(long n) {
+    public static long solution(long n) {
+        //TODO 입력값을 스트링으로 변환하여 배열을 만든다.
         String input = String.valueOf(n);
         String[] split = input.split("");
+        //TODO 배열을 내림차순으로 정렬한 후
+        Arrays.sort(split, Comparator.reverseOrder());
+        //TODO 각 값을 더해 인트로 변환한다.
+        String result ="";
+        for(String a:split)
+           result += a;
 
-        int[] answer = new int[split.length];
-        for(int i=0; i<split.length; i++){
-            answer[i]= Integer.parseInt(split[split.length-i-1]);
-        }
-        return answer;
+        return Long.parseLong(result);
     }
 }

8f5fa9e632fd3e3a797dca1c2ace9bbb385c2a0c
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-28 16:24:59 +0900

    이상한 문자 만들기 실패코드

--- src/programmers/level1/Solution.java
+++ src/programmers/level1/Solution.java
@@ -1,28 +1,32 @@
 package programmers.level1;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Stack;
 
-public class Solution {
-
-    public static int[] solution(int []arr) {
-        List<Integer> list = new ArrayList<>();
-
-        list.add(arr[0]);
-        for(int i=0; i<arr.length-1; i++){
-            if(arr[i]!=arr[i+1])
-                list.add(arr[i+1]);
+class Solution {
+    public static String solution(String s) {
+        //TODO 1) 띄어쓰기 단위로 쪼개서 배열에 담는다.
+        String[] s1 = s.trim().split(" ");
+        //TODO 2) 배열 인덱스 하나씩 Char 의 형태로 쪼개 대소문자를 구분한다.
+        List<Character> arr = new ArrayList<>();
+        for(int i=0; i<s1.length; i++){
+            for(int j=0; j<s1[i].length(); j++){
+                if ((j % 2 == 0)) {
+                    arr.add(Character.toUpperCase(s1[i].charAt(j)));
+                } else {
+                    arr.add(Character.toLowerCase(s1[i].charAt(j)));
+                }
+            }
+            arr.add(' ');
         }
-        int[] answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++)
-            answer[i]=list.get(i);
+        String answer = "";
+        for(int i =0; i<arr.size()-1; i++){
+            answer += arr.get(i);
+        }
         return answer;
     }
-
     public static void main(String[] args) {
-        int[] solution = Solution.solution(new int[]{1, 1, 3, 3, 0, 1, 1});
-        System.out.println(Arrays.toString(solution));
+        String example = "he llo  world  i m siyoung";
+        System.out.println(Solution.solution(example));
     }
 }

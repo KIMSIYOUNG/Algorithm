2e4bdba05a8c0a8277f5cbfbd07d82aaa17a6dfb
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-28 15:10:36 +0900

    같은 숫자는 싫어

--- src/programmers/level1/Solution.java
+++ src/programmers/level1/Solution.java
@@ -1,37 +1,28 @@
 package programmers.level1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
 
-class Solution {
+public class Solution {
 
-    //TODO 97-122 소문자 65-90 대문자.
-    //TODO 경우의 수 : 1) 소문자인데 122보다 큰 경우 2) 대문자인데 90보다 큰 경우 3) 나머지 경우
-    public static String solution(String s, int n) {
-        char[] s_char = s.toCharArray();
+    public static int[] solution(int []arr) {
+        List<Integer> list = new ArrayList<>();
 
-        for(int i=0; i<s.length(); i++){
-            if(s_char[i]==' ')
-                s_char[i]= ' ';
-            else{
-                if((int)s_char[i]+n>122&&(int)s_char[i]>=97&&(int)s_char[i]<=122)
-                    s_char[i] = (char) (s_char[i]+n-26);
-                else if((int)s_char[i]>=65&&(int)s_char[i]+n>90&&(int)s_char[i]<=90)
-                    s_char[i] = (char)(s_char[i]+n-26);
-                else
-                    s_char[i] += n;
-            }
-
+        list.add(arr[0]);
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i]!=arr[i+1])
+                list.add(arr[i+1]);
         }
-
-        String answer ="";
-        for(int i=0; i<s_char.length; i++)
-            answer +=s_char[i];
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++)
+            answer[i]=list.get(i);
         return answer;
     }
 
     public static void main(String[] args) {
-        String ab = Solution.solution("a B z", 4);
-        System.out.println(ab);
-
+        int[] solution = Solution.solution(new int[]{1, 1, 3, 3, 0, 1, 1});
+        System.out.println(Arrays.toString(solution));
     }
 }

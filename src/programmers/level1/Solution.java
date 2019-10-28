9eccc27524f07300fc80ae3a25159197095070d2
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-28 12:40:16 +0900

    내마음대로 정렬하기

--- src/programmers/level1/Solution.java
+++ src/programmers/level1/Solution.java
@@ -1,24 +1,37 @@
 package programmers.level1;
 
+import java.util.Arrays;
+
 class Solution {
-    public static String solution(int n) {
-        String subak = "수박";
-        String baksu = "박수";
-        String answer ="";
-        if(n%2==0){
-            for(int i=0; i<n/2; i++){
-                answer += subak;
+
+    //TODO 97-122 소문자 65-90 대문자.
+    //TODO 경우의 수 : 1) 소문자인데 122보다 큰 경우 2) 대문자인데 90보다 큰 경우 3) 나머지 경우
+    public static String solution(String s, int n) {
+        char[] s_char = s.toCharArray();
+
+        for(int i=0; i<s.length(); i++){
+            if(s_char[i]==' ')
+                s_char[i]= ' ';
+            else{
+                if((int)s_char[i]+n>122&&(int)s_char[i]>=97&&(int)s_char[i]<=122)
+                    s_char[i] = (char) (s_char[i]+n-26);
+                else if((int)s_char[i]>=65&&(int)s_char[i]+n>90&&(int)s_char[i]<=90)
+                    s_char[i] = (char)(s_char[i]+n-26);
+                else
+                    s_char[i] += n;
             }
-        }else{
-            answer ="수";
-            for(int i=0; i<n/2; i++)
-                answer += baksu;
+
         }
+
+        String answer ="";
+        for(int i=0; i<s_char.length; i++)
+            answer +=s_char[i];
         return answer;
     }
 
     public static void main(String[] args) {
-        String solution = Solution.solution(7);
-        System.out.println(solution);
+        String ab = Solution.solution("a B z", 4);
+        System.out.println(ab);
+
     }
 }

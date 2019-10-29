bc524d08f3d7bbdf676a3a44cfbdcecf0d79c879
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-29 11:42:33 +0900

    문자열 내마음대로 정렬하기2

--- src/programmers/level1/Solution.java
+++ src/programmers/level1/Solution.java
@@ -1,39 +1,23 @@
 package programmers.level1;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 class Solution {
-    public static String[] solution(String[] strings, int n) {
-        Arrays.sort(strings);
+    public String[] solution(String[] strings, int n) {
+        List<String> list = new ArrayList<>();
 
-        Map<Integer,Character> map = new HashMap<>();
-        for(int i=0; i<strings.length; i++){
-            map.put(i,strings[i].charAt(n));
+        for(int i = 0; i<strings.length; i++){
+            list.add(strings[i].charAt(n)+strings[i]);
         }
-        Iterator<Integer> it = sortByValue(map).iterator();
+        Collections.sort(list);
 
-        String[] answer = new String[strings.length];
-        for(int i=0; it.hasNext(); i++){
-            answer[i]=strings[it.next()];
+        String[] answer = new String[list.size()];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = list.get(i).substring(1,list.get(i).length());
         }
         return answer;
-    }
 
-    private static List<Integer> sortByValue(Map<Integer, Character> map) {
-        List<Integer> list = new ArrayList<Integer>();
-        list.addAll(map.keySet());
-        Collections.sort(list, new Comparator<Object>(){
-            public int compare(Object o1, Object o2){
-                Object v1  = map.get(o1);
-                Object v2 = map.get(o2);
-                return ((Comparable<Object>)v1).compareTo(v2);
-            }
-        });
-        return list;
-    }
-
-    public static void main(String[] args) {
-        String[] solution = Solution.solution(new String[]{"sun", "bed", "car"}, 1);
-        System.out.println(Arrays.toString(solution));
     }
 }

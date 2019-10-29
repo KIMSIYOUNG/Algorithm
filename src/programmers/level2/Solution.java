7ac2c15b5dbbb928a511026611694268919cd7f3
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-29 20:55:55 +0900

    124나라

--- src/programmers/level2/Solution.java
+++ src/programmers/level2/Solution.java
@@ -1,26 +1,17 @@
 package programmers.level2;
 
-import java.util.Arrays;
-
 class Solution {
-    public static int[] solution(int[] heights) {
-        //        6,9,5,7,4;
-        //0->0개 1->1개 2->2개 3은 3개 4는 4개
-        int[] answer = new int[heights.length];
-        for(int i=0; i<heights.length; i++){
-            int max = 0;
-            for(int k=0; k<i; k++){
-                if(heights[k]>heights[i])
-                    max = k+1;
-            }
-            answer[i] = max;
+    public static String solution(int n) {
+        String answer = "";
+        String[] result = new String[]{"4", "1", "2"};
+        while (n > 0) {
+            answer = result[n % 3] + answer;
+            n = (n - 1) / 3;
         }
         return answer;
     }
 
     public static void main(String[] args) {
-        int[] solution = Solution.solution(new int[]{1, 5, 3, 6, 7, 6 ,5 });
-        System.out.println(Arrays.toString(solution));
-
+        System.out.println(Solution.solution(4));
     }
 }

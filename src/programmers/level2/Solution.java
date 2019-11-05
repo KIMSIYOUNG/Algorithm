c733f8b375a9a10f0dfe90e7f365ac539ad15433
KIMSIYOUNG <tldud2404@gmail.com>
2019-11-05 16:37:29 +0900

    문자열 정렬(Comparator)

--- src/programmers/level2/Solution.java
+++ src/programmers/level2/Solution.java
@@ -1,38 +1,33 @@
 package programmers.level2;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
-    public static int solution(int[] priorities, int location) {
-        int answer = 1;
-
-        PriorityQueue priority = new PriorityQueue<>(Collections.reverseOrder());
-
-        for (int task : priorities) {
-            priority.add(task);
-        }
-        //{2,5,4,1,3};
-
-        while (!priority.isEmpty()) {
-            for (int i = 0; i < priorities.length; i++) {
-                if (priorities[i] == (int) priority.peek()) {
-                    if (i == location) {
-                        return answer;
-                    }
-                    priority.poll();
-                    answer++;
-                }
+    public static String solution(int[] numbers) {
+        /** 1)선언부  **/
+        String answer = "";
+        String[] str = new String[numbers.length];
+        for(int i=0; i<numbers.length; i++)
+            str[i]=String.valueOf(numbers[i]);
+        /** 2)정렬 **/
+        Arrays.sort(str, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2+o1).compareTo(o1+o2);
             }
+        });
+        if(str[0].startsWith("0"))
+            answer += 0;
+        else{
+            for(int i=0; i<str.length; i++)
+                answer += str[i];
         }
-
         return answer;
     }
 
     public static void main(String[] args) {
-        int solution = Solution.solution(new int[]{2, 1, 3, 2, 3, 4}, 4);
+        String solution = solution(new int[]{3,30,34,5,9});
         System.out.println(solution);
     }
 }

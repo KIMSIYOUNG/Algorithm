043bb00604fa3a60180dbb530a79074091532463
KIMSIYOUNG <tldud2404@gmail.com>
2019-11-06 18:23:28 +0900

    최솟값 만들기 L2

--- src/programmers/level2/Solution.java
+++ src/programmers/level2/Solution.java
@@ -1,25 +1,24 @@
 package programmers.level2;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 class Solution {
-    public static String solution(String s) {
-        int max = 0; int min = 0; String answer = "";
-        String[] s1 = s.split(" ");
-        int[] ints = new int[s1.length];
-        for (int i = 0; i < ints.length; i++) {
-            ints[i] = Integer.parseInt(s1[i]);
+    public static int solution(int []A, int []B) {
+        int answer = 0;
+        Integer[] arr = new Integer[B.length];
+        for(int i=0; i<arr.length; i++)
+            arr[i] = B[i];
+        Arrays.sort(A);
+        Arrays.sort(arr, Collections.reverseOrder());
+        for(int i=0; i<A.length; i++){
+            answer += A[i] * arr[i];
         }
-        Arrays.sort(ints);
-        max = ints[0];
-        min = ints[ints.length-1];
-        answer = String.valueOf(max)+" "+String.valueOf(min);
         return answer;
     }
 
     public static void main(String[] args) {
-        System.out.println(solution("1 2 3 4"));
-        System.out.println(solution("-1 -2 -3 -4"));
-        System.out.println(solution("-1 -1"));
+        System.out.println(solution(new int[]{1,4,2},new int[]{5,4,4}));
+        System.out.println(solution(new int[]{1,2,},new int[]{3,4}));
     }
 }

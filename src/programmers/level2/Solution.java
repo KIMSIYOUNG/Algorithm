321f5479ced08ef54ac7c54252806da435418dd2
KIMSIYOUNG <tldud2404@gmail.com>
2019-11-06 20:20:31 +0900

    다음 큰 숫자 L2

--- src/programmers/level2/Solution.java
+++ src/programmers/level2/Solution.java
@@ -1,31 +1,27 @@
 package programmers.level2;
 
+import java.util.Arrays;
+
 class Solution {
     public static int solution(int n) {
-        int count = 0;
-        int i=1;
-        int answer = i;
-        int k=i+1;
-
-        while(true){
-            if(i==n){
-                count++;
+        int answer = 0;
+        String[] s = Integer.toBinaryString(n).split("");
+        int s_count = 0;
+        for(int i =0; i<s.length; i++)
+            if(s[i].equals("1"))
+                s_count++;
+        for(int i=n+1; i<=1000000; i++){
+            String[] split = Integer.toBinaryString(i).split("");
+            int count =0;
+            for(int k=0; k<split.length; k++)
+                if(split[k].equals("1"))
+                    count++;
+            if(s_count==count){
+                answer = i;
                 break;
             }
-            answer += k;
-            if(answer==n){
-                count++; i++;
-                k = i+1; answer = i;
-            }
-            else{
-                k++;
-                if(answer>n){
-                    i++; k = i+1;
-                    answer = i;
-                }
-            }
         }
-        return count;
+        return answer;
     }
 
     public static void main(String[] args) {

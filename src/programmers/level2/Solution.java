8f36fca6982f5be3796e97abc4f1721929e9fb15
KIMSIYOUNG <tldud2404@gmail.com>
2019-11-06 20:21:50 +0900

    다음 큰 숫자 Integer.bitCount 이용

--- src/programmers/level2/Solution.java
+++ src/programmers/level2/Solution.java
@@ -5,17 +5,10 @@ import java.util.Arrays;
 class Solution {
     public static int solution(int n) {
         int answer = 0;
-        String[] s = Integer.toBinaryString(n).split("");
-        int s_count = 0;
-        for(int i =0; i<s.length; i++)
-            if(s[i].equals("1"))
-                s_count++;
+        int s_count = Integer.bitCount(n);
+
         for(int i=n+1; i<=1000000; i++){
-            String[] split = Integer.toBinaryString(i).split("");
-            int count =0;
-            for(int k=0; k<split.length; k++)
-                if(split[k].equals("1"))
-                    count++;
+            int count =Integer.bitCount(i);
             if(s_count==count){
                 answer = i;
                 break;

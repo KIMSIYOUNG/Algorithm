e6d0e1e17c672072e5c6299d70cc9e29677b4ddb
KIMSIYOUNG <tldud2404@gmail.com>
2019-11-08 11:12:18 +0900

    최소공배수 배열

--- src/programmers/level2/Solution.java
+++ src/programmers/level2/Solution.java
@@ -1,19 +1,35 @@
 package programmers.level2;
 
+import java.util.Arrays;
+
 class Solution {
-    public static String solution(String s) {
-        String answer = "";
-        String[] split = s.toLowerCase().split("");
-        boolean flag = true;
-        for(String ss : split){
-            answer += flag?ss.toUpperCase():ss;
-            flag = ss.equals(" ")?true:false;
+    public static int solution(int[] arr) {
+        Arrays.sort(arr);
+        int answer = arr[0];
+        int small = 0;
+        int big = 0;
+        for(int i=1; i<arr.length; i++){
+            small = Math.min(answer,arr[i]);
+            big = Math.max(answer,arr[i]);
+
+            answer = small * big / gcd(small,big);
         }
         return answer;
     }
-//97 122 65 90      65 90 97 122
-    public static void main(String[] args) {
-        String for_the_last_week = solution("3people unFollowed me");
-        System.out.println(for_the_last_week);
+
+    private static int gcd(int small, int big) {
+        int answer = 0;
+        for(int i=1; i<=small; i++){
+            if(small%i==0 &&big%i==0)
+                answer = i;
+        }
+        return answer;
     }
+
+    public static void main(String[] args) {
+        int solution = solution(new int[]{5,10,15});
+        System.out.println(solution);
+    }
+
+
 }
